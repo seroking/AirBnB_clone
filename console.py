2134fe4b6a4fdfdf568f6e8498331cc97ef71cee
seroking <baridmohamedamine004@gmail.com>
2023-10-10 03:10:15 -0700

    Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" Console 0.0.1 """
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ cmd class """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Quit command to exist the program """
+        return True
+
+    def do_EOF(self):
+        """ EOF command to exist the program"""
+        return True
+
+    def emptyline(self):
+        """ skip empty line"""
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
